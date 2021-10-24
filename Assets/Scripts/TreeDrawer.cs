1181d8a0004c658f7332f277ec6dcf9e488a84f5
SkullAG <aitormine12@gmail.com>
2021-10-24 18:06:42 +0200

    Project added

---
+++ Assets/Scripts/TreeDrawer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeDrawer : MonoBehaviour
+{
+    public float anchoCopa;
+    public float latoCopa;
+    public float anchoTronco;
+    public float latoTronco;
+    public Vector3 posicionBase;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
